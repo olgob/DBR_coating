b6476795b3746931403737b5b54f9c7269ca24e2
Olivier <gobron.olivier@gmail.com>
2019-10-01 18:23:25 +0200

    Preparation to put the package on PyPi

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dbr_coating',
+      version='0.1',
+      description='Distributed Bragg reflector calculator',
+      packages=['dbr_coating'],
+      zip_safe=False)
