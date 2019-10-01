7bf1e68724dc98bdfc1b7b5928c14d84b7bae0e8
Olivier <gobron.olivier@gmail.com>
2019-10-01 12:18:31 +0200

    code cleaning and simplification of DBR_coating_simulation.py

--- DBR_coating_simulation.py
+++ DBR_coating_simulation.py
@@ -1,69 +1,66 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import ABCD_matrix
-import stack
+from ABCD_matrix import transfer_matrix_method
+from stack import Stack
 
 
-def loadCoatingData(filename):
-    SiO2Index = 1.457
-    Ta2O5Index = 2.071
-    airIndex = 1
-    substrateIndex = 1.46
+def get_spectral_response(wavelengths, stack):
+    """
+        Realize ABCD matrices calculation to get the spectral response of a coating stack
 
-    layers_data = np.loadtxt(filename, dtype='str')
-    layerNumber = layers_data[:, 0].astype(int)
-    layerMaterial = layers_data[:, 1]
-    layerThickness = layers_data[:, 2].astype(float)
-    layerThickness = layerThickness * 1e-9
-    layerIndex = np.zeros_like(layerMaterial, dtype=float)
-    n = 0
-    while n < len(layerIndex):
-        if layerMaterial[n] == 'SiO2':
-            layerIndex[n] = SiO2Index
-        elif layerMaterial[n] == 'Ta2O5':
-            layerIndex[n] = Ta2O5Index
-        elif layerMaterial[n] == 'Air':
-            layerIndex[n] = airIndex
-        elif layerMaterial[n] == 'Substrate':
-            layerIndex[n] = substrateIndex
-        else:
-            layerIndex[n] = 0
-        n += 1
-    return layerIndex, layerThickness
+            Args:
+                wavelengths (array): wavelengths for which the spectral response is calculated.
+                stack (object) : Stack that defines the index and the thickness of each layer of the coating.
 
+            Returns:
+                reflectivity_te (array) : reflectivity for TE polarized light for each wavelengths specified
+                transmission_te (array) : transmission for TE polarized light for each wavelengths specified
+        """
 
-def get_coating_result(wavelengths, stack):
     resolution = 1
     for i, re_index in enumerate(stack.index):
         step_size = stack.thickness.sum() / 2**17
         z0 = np.linspace(0, stack.thickness[i], round(stack.thickness[i] / step_size))
         resolution += len(z0)
-    EtotTE = np.zeros([resolution, wavelengths.size], dtype=complex)
-    EtotTM = np.zeros([resolution, wavelengths.size], dtype=complex)
-    RefTE = np.zeros(wavelengths.size, dtype=complex)
-    RefTM = np.zeros(wavelengths.size, dtype=complex)
-    TraTE = np.zeros(wavelengths.size, dtype=complex)
-    TraTM = np.zeros(wavelengths.size, dtype=complex)
-    ntot = np.zeros([resolution, wavelengths.size], dtype=complex)
-    thetatot = np.zeros([len(stack.index) + 1, wavelengths.size], dtype=complex)
 
-    A0 = 1
-    B0 = 0
-    theta = 0
+    electric_tot_te = np.zeros([resolution, len(wavelengths)], dtype=complex)
+    electric_tot_tm = np.zeros([resolution, len(wavelengths)], dtype=complex)
+    reflectivity_te = np.zeros(len(wavelengths), dtype=complex)
+    reflectivity_tm = np.zeros(len(wavelengths), dtype=complex)
+    transmission_te = np.zeros(len(wavelengths), dtype=complex)
+    transmission_tm = np.zeros(len(wavelengths), dtype=complex)
+    index_tot = np.zeros([resolution, len(wavelengths)], dtype=complex)
+    theta_tot = np.zeros([len(stack.index) + 1, wavelengths.size], dtype=complex)
+
+    a0 = 1  # Initial amplitude of electric field going toward the coating
+    b0 = 0  # Initial amplitude of electric field going back the coating (if 0, no counter propagating light)
+    theta = 0  # angle of the beam with respect to the coating
+
     for i, lam in enumerate(wavelengths):
-        print(i / 1601 * 100, '%')
-        EtotTE[:, i], EtotTM[:, i], RefTE[i], RefTM[i], TraTE[i], TraTM[i], ntot, L, thetatot \
-            = ABCD_matrix.transfer_matrix_method(stack, A0, B0, lam, theta)
-    return RefTE, TraTE, 1 - (RefTE + TraTE)
+        electric_tot_te[:, i], electric_tot_tm[:, i], reflectivity_te[i], reflectivity_tm[i], transmission_te[i], \
+        transmission_tm[i], index_tot, L, theta_tot = transfer_matrix_method(stack, a0, b0, lam, theta)
+    return reflectivity_te, transmission_te, 1 - (reflectivity_te + transmission_te)
 
 
-n_DTUflat_mirror, L_DTUflat_mirror = loadCoatingData('DTU_flat_coating.txt')
-DTU_DBR_stack_flat = stack.Stack(index_arr=n_DTUflat_mirror, thickness_arr=L_DTUflat_mirror)
-DTU_DBR_stack_flat_reversed = stack.Stack(index_arr=n_DTUflat_mirror[::-1], thickness_arr=L_DTUflat_mirror[::-1])
-wavelengths = np.linspace(550e-9, 750e-9, 1601)
-RefTE, TraTE, LossTE = get_coating_result(wavelengths, DTU_DBR_stack_flat)
-# RefTE_reversed, TraTE_reversed, LossTE_reversed = get_coating_result(wavelengths, DTU_DBR_stack_flat_reversed)
+def load_stack(filename):
+    data = np.genfromtxt(filename, skip_header=1)
+    index_arr = data[:, 2]
+    thickness_arr = data[:, 3] / 1e9
+    stack = Stack(index_arr, thickness_arr)
+    return stack
 
-plt.plot(wavelengths * 1e9, RefTE)
-# plt.plot(wavelengths*1e9, RefTE_reversed, 'r--')
+
+coating = load_stack('stack_example.txt')
+wl = np.linspace(500e-9, 800e-9, 401)
+reflectivity_te, transmission_te, loss_te = get_spectral_response(wl, coating)
+
+fig, ax = plt.subplots()
+ax.plot(wl * 1e9, np.real(reflectivity_te), label='reflectivity')
+ax.plot(wl * 1e9, np.real(transmission_te), label='transmission')
+ax.plot(wl * 1e9, np.real(loss_te), label='losses')
+ax.set_title('Spectral response')
+ax.set_xlabel('Wavelength [nm]')
+ax.set_ylabel('Amplitude')
+ax.grid(color='black', linestyle='--', linewidth=0.5)
+ax.legend(loc='upper right')
 plt.show()
